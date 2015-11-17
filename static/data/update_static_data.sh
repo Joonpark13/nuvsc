733ded09e39b004e7a03f6f4b4c04b55f62f6da7
Joon Park <joon.park13@gmail.com>
2015-11-17 03:08:19 -0600

    data update

---
+++ static/data/update_static_data.sh
@@ -0,0 +1,19 @@
+wget http://localhost:5000/current_term
+mv current_term current_term.json
+wget http://localhost:5000/all_terms
+mv all_terms all_terms.json
+wget http://localhost:5000/all_schools
+mv all_schools all_schools.json
+wget http://localhost:5000/all_subjects
+mv all_subjects  all_subjects.json
+wget http://localhost:5000/all_courses
+mv all_courses all_courses.json
+wget http://localhost:5000/all_sections
+mv all_sections  all_sections.json
+wget http://localhost:5000/all_components
+mv all_components  all_components.json
+wget http://localhost:5000/all_descriptions
+mv all_descriptions  all_descriptions.json
+wget http://localhost:5000/indexedDBversion
+wget http://localhost:5000/search_data/4610
+mv 4610 search_data_4610.json
